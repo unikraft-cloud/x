60a700d2076ee36e6c62b987bd99c23b327fe0ae
Alexander Jung <alex@unikraft.com>
2025-09-30 17:40:18 +0200

    feat: Add `guesstermwidth` module
    Signed-off-by: Alexander Jung <alex@unikraft.com>

---
+++ guesstermwidth/guesstermwidth.go
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2025, Unikraft GmbH and The Unikraft CLI Authors.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+//go:build appengine || (!linux && !freebsd && !darwin && !dragonfly && !netbsd && !openbsd)
+// +build appengine !linux,!freebsd,!darwin,!dragonfly,!netbsd,!openbsd
+
+package utils
+
+import "io"
+
+// GuessTermWidth returns a default terminal width of 80 characters since the
+// environment does not support querying terminal dimensions.
+func GuessTermWidth(w io.Writer) int {
+	return 80
+}
