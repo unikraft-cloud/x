405595b1322efcc875aca958b43a129ea7b380a4
Justin Chadwell <justin@unikraft.com>
2026-04-14 12:37:03 +0100

    feat(telemetry): Add centralized OTEL telemetry package
    Not just for logging anymore, but for tracing and metrics as well now!
    
    This new package initializes all telemetry (splitting most of it out of
    the log package), while the new middleware adds spans and metrics and
    links logs to every new request.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- middleware/go.mod
+++ middleware/go.mod
@@ -1,6 +1,6 @@
 module unikraft.com/x/middleware
 
-go 1.25.5
+go 1.26.1
 
 require (
 	codeberg.org/gruf/go-byteutil v1.3.0
@@ -9,6 +9,9 @@ require (
 	github.com/getsentry/sentry-go/gin v0.44.1
 	github.com/gin-contrib/cors v1.7.7
 	github.com/gin-gonic/gin v1.12.0
+	go.opentelemetry.io/otel v1.43.0
+	go.opentelemetry.io/otel/metric v1.43.0
+	go.opentelemetry.io/otel/trace v1.43.0
 	unikraft.com/x/log v0.0.0-20250930151222-cdcd2dd742c9
 )
 
@@ -17,11 +20,14 @@ require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/gabriel-vasile/mimetype v1.4.12 // indirect
 	github.com/getsentry/sentry-go/zerolog v0.33.0 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.30.1 // indirect
@@ -42,6 +48,7 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	golang.org/x/arch v0.23.0 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
---
+++ telemetry/telemetry.go
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2026, Unikraft GmbH.
+// Licensed under the BSD-3-Clause License (the "License").
+// You may not use this file except in compliance with the License.
+
+package telemetry
+
+import (
+	"context"
+	"errors"
+	"os"
+	"strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	otellog "go.opentelemetry.io/otel/log/global"
+	"go.opentelemetry.io/otel/propagation"
+	sdklog "go.opentelemetry.io/otel/sdk/log"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+// Init configures OTEL tracing, metrics, and logging exporters.
+// Respects OTEL_SDK_DISABLED, OTEL_TRACES_EXPORTER, OTEL_METRICS_EXPORTER, and OTEL_LOGS_EXPORTER.
+func Init(ctx context.Context) (func(context.Context) error, error) {
+	noop := func(context.Context) error { return nil }
+	disabled, _ := strconv.ParseBool(os.Getenv("OTEL_SDK_DISABLED"))
+	if disabled {
+		return noop, nil
+	}
+
+	res, err := resource.New(ctx,
+		resource.WithFromEnv(),
+		resource.WithTelemetrySDK(),
+		resource.WithHost(),
+	)
+	if err != nil {
+		return noop, err
+	}
+	if res == nil {
+		res = resource.Empty()
+	}
+
+	var shutdownFuncs []func(context.Context) error
+
+	tracesExporter := os.Getenv("OTEL_TRACES_EXPORTER")
+	switch tracesExporter {
+	case "", "otlp":
+		traceExporter, err := otlptracehttp.New(ctx)
+		if err != nil {
+			return noop, err
+		}
+		tracerProvider := sdktrace.NewTracerProvider(
+			sdktrace.WithBatcher(traceExporter),
+			sdktrace.WithResource(res),
+		)
+		otel.SetTracerProvider(tracerProvider)
+		shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	case "none":
+		// No trace exporter
+	default:
+		return noop, errors.New("OTEL_TRACES_EXPORTER=" + tracesExporter + " is not supported")
+	}
+
+	metricsExporter := os.Getenv("OTEL_METRICS_EXPORTER")
+	switch metricsExporter {
+	case "", "otlp":
+		metricExporter, err := otlpmetrichttp.New(ctx)
+		if err != nil {
+			// Cleanup previously initialized providers
+			for _, shutdown := range shutdownFuncs {
+				_ = shutdown(ctx)
+			}
+			return noop, err
+		}
+		meterProvider := sdkmetric.NewMeterProvider(
+			sdkmetric.WithReader(sdkmetric.NewPeriodicReader(metricExporter)),
+			sdkmetric.WithResource(res),
+		)
+		otel.SetMeterProvider(meterProvider)
+		shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	case "none":
+		// No metric exporter
+	default:
+		return noop, errors.New("OTEL_METRICS_EXPORTER=" + metricsExporter + " is not supported")
+	}
+
+	logsExporter := os.Getenv("OTEL_LOGS_EXPORTER")
+	switch logsExporter {
+	case "", "otlp":
+		logExporter, err := otlploghttp.New(ctx)
+		if err != nil {
+			// Cleanup previously initialized providers
+			for _, shutdown := range shutdownFuncs {
+				_ = shutdown(ctx)
+			}
+			return noop, err
+		}
+		logProvider := sdklog.NewLoggerProvider(
+			sdklog.WithResource(res),
+			sdklog.WithProcessor(sdklog.NewBatchProcessor(logExporter)),
+		)
+		otellog.SetLoggerProvider(logProvider)
+		shutdownFuncs = append(shutdownFuncs, logProvider.Shutdown)
+	case "none":
+		// No log exporter
+	default:
+		return noop, errors.New("OTEL_LOGS_EXPORTER=" + logsExporter + " is not supported")
+	}
+
+	// Set propagator if any telemetry is enabled
+	if len(shutdownFuncs) > 0 {
+		otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		))
+	}
+
+	shutdown := func(ctx context.Context) error {
+		var shutdownErr error
+		for _, fn := range shutdownFuncs {
+			if err := fn(ctx); err != nil {
+				shutdownErr = errors.Join(shutdownErr, err)
+			}
+		}
+		return shutdownErr
+	}
+	return shutdown, nil
+}
