f278a82e8bcb8e5fd7f2b29b380a422800c9d7fb
Justin Chadwell <justin@unikraft.com>
2026-05-01 14:38:54 +0100

    feat: Inject package info into templates
    And allow filtering by the package.
    
    Signed-off-by: Justin Chadwell <justin@unikraft.com>

--- tools/openapi-gen/generator.go
+++ tools/openapi-gen/generator.go
@@ -123,22 +123,49 @@ func NewGenerator(specPath string, vars map[string]string, templateDir string) (
 
 	Preprocess(parser.doc, parser)
 
-	tmpl, templateNames, err := loadTemplates(parser, templateDir)
+	g := &Generator{
+		parser:     parser,
+		vars:       vars,
+		operations: parser.ParseOperations(),
+		models:     parser.ParseModels(),
+	}
+
+	tmpl, templateNames, err := loadTemplates(parser, templateDir, &g.models, &g.vars)
 	if err != nil {
 		return nil, err
 	}
+	g.templates = tmpl
+	g.templateNames = templateNames
 
-	return &Generator{
-		parser:        parser,
-		templates:     tmpl,
-		templateNames: templateNames,
-		vars:          vars,
-		operations:    parser.ParseOperations(),
-		models:        parser.ParseModels(),
-	}, nil
+	return g, nil
 }
 
-func loadTemplates(parser *Parser, templateDir string) (*template.Template, []string, error) {
+// FilterByPackage keeps only operations and models whose x-package matches
+// the given package name.
+func (g *Generator) FilterByPackage(pkg string) {
+	// Filter operations
+	var filteredOps []PathOperation
+	for _, op := range g.operations {
+		if op.Operation == nil {
+			continue
+		}
+		if opPkg, _ := op.Operation.Extensions["x-package"].(string); opPkg == pkg {
+			filteredOps = append(filteredOps, op)
+		}
+	}
+	g.operations = filteredOps
+
+	// Filter models
+	var filteredModels []Model
+	for _, m := range g.models {
+		if m.Package == pkg {
+			filteredModels = append(filteredModels, m)
+		}
+	}
+	g.models = filteredModels
+}
+
+func loadTemplates(parser *Parser, templateDir string, models *[]Model, vars *map[string]string) (*template.Template, []string, error) {
 	if templateDir == "" {
 		return nil, nil, fmt.Errorf("template directory not specified")
 	}
@@ -152,7 +179,7 @@ func loadTemplates(parser *Parser, templateDir string) (*template.Template, []st
 		return nil, nil, fmt.Errorf("no templates found in %s", templateDir)
 	}
 
-	tmpl := template.New("").Funcs(templateFuncs{parser}.Funcs())
+	tmpl := template.New("").Funcs((&templateFuncs{parser: parser, models: models, vars: vars}).Funcs())
 
 	if _, err := tmpl.ParseFiles(files...); err != nil {
 		return nil, nil, fmt.Errorf("loading template overrides: %w", err)
